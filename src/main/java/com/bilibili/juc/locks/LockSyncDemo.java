03c070d118d9a881c72f176120852698b126d7ad
zhangbingbing <397525017@qq.com>
2022-08-08 18:43:10 +0800

    ds

--- src/main/java/com/bilibili/juc/locks/LockSyncDemo.java
+++ src/main/java/com/bilibili/juc/locks/LockSyncDemo.java
@@ -7,33 +7,31 @@ import java.awt.print.Book;
  * @auther zzyy
  * @create 2022-01-18 15:56
  */
-public class LockSyncDemo
-{
+public class LockSyncDemo {
 
     Object object = new Object();
 
-    public void m1()
-    {
-        synchronized (object)
-        {
+    public void m1() {
+        synchronized (object) {
             System.out.println("----hello synchronized code block");
-            throw new RuntimeException("-----exp");
+            //throw new RuntimeException("-----exp");
         }
     }
 
-    public synchronized void m2()
-    {
+    public synchronized void m2() {
         System.out.println("----hello synchronized m2");
     }
 
-    public static synchronized void m3()
-    {
+    public static synchronized void m3() {
         System.out.println("----hello static synchronized m3");
     }
 
+    public void m4() {
+        System.out.println("----hello m4");
+    }
 
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
 
     }
 }
